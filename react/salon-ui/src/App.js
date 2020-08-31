11ce51d030d3531926eef2e8a006080c94e7e4d2
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2020-08-31 10:57:12 +0100

    Step 1 update react from reference

--- react/salon-ui/src/App.js
+++ react/salon-ui/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-light bg-dark">
-        <a className="navbar-brand text-light">AR Salon and Day Spa Services</a>
+      <nav className="navbar navbar-light bg-dark navbar-expand-md">
+        <a className="navbar-brand text-light" href="/">AR Salon and Day Spa Services</a>
       </nav>
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <main role="main" className="container">
+          <div className="padding-container">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+
+          </div>
+        </main>
        
     </div>
   );
