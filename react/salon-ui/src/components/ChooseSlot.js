8154db40669b437b4ce293af198dcd83e859a186
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2020-09-25 16:50:50 +0100

    React part 3 add form

--- react/salon-ui/src/components/ChooseSlot.js
+++ react/salon-ui/src/components/ChooseSlot.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 
@@ -13,9 +14,19 @@ class ChooseSlot extends Component {
 
     render() {
         return (
-            <div>
-                <h1>{this.props.match.params.serviceId} - {this.props.match.params.serviceName}</h1>
-            </div>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Label>Choose a Date for {this.props.match.params.serviceName}</Form.Label>
+                    </Col>
+                    <Col>
+                        <Form.Control as="input"></Form.Control>
+                    </Col>
+                    <Col>
+                        <Button type="submit">Show Slots</Button>
+                    </Col>
+                </Row>
+            </Form>
         )
     }
 }
